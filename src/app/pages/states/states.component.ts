d9e8b0c7f90ec186414e9c9453d10643af2f4fe8
K S Sayhan <sayhansdhr1115@gmail.com>
2024-12-18 23:33:52 +0530

    Started with the basic steps in Creating basic components 18-12-24

---
+++ src/app/pages/states/states.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../ui/navbar/navbar.component";
+
+@Component({
+  selector: 'app-states',
+  imports: [NavbarComponent],
+  templateUrl: './states.component.html',
+  styleUrl: './states.component.scss'
+})
+export class StatesComponent {
+
+}
